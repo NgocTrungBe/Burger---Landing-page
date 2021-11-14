965e6e9797f33ae76202b756efa8bbe0463bb504
NgocTrungBe <bengoctrung23@gmail.com>
2021-11-14 16:39:13 +0700

    banner, products responsive

--- js/script.js
+++ js/script.js
@@ -11,3 +11,7 @@ menu.addEventListener("click", function() {
     this.classList.toggle("fa-times");
     nav.classList.toggle("menu-toggle");
 })
+
+const banner = document.querySelector(".banner");
+const banner__single_item = document.querySelectorAll(".banner__single-item img");
+console.log(banner__single_item[0].clientWidth)
